67eacc05471ae4cc86867942aa2a86e3803d414b
ujwal49 <ujwalreddykotla111@gmail.com>
2022-08-25 21:25:18 +0530

    change bg color to white and make text color variable

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 let i = 0;
 const colors = ["#9400D3", "#4B0082", "#0000FF", "#00FF00", "#FFFF00", "#FF7F00", "#FF0000"]                        
 
+const text = document.getElementById("body-text")
+
 function changeBg(){
-  document.body.style.backgroundColor = colors[i]
+  text.style.color = colors[i]
   i = i + 1;
   if(i == 7) i = 0;
 }
 
-setInterval(changeBg, 250);
+setInterval(changeBg, 480);
